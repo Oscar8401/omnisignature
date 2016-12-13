9edfe26be2396d33ed9bd826c163942dc14193d0
Travis LaDuke <travisladuke@gmail.com>
2016-12-13 10:16:42 -0800

    Set up jsfmt.
    This formats JSX correctly for me, and adds a couple other nice things.

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React, {
+  Component
+} from 'react';
+import {
+  Row,
+  Col
+} from 'react-bootstrap';
+import classNames from 'classnames';
+
+export const Header = (state) => (
+  <Row>
+<Col>
+<h1 className={'text-center'}>DotLink</h1>
+</Col>
+<Col>
+<h2 className={'text-center'}>Email Signature Creator</h2>
+</Col>
+<Col>
+<h4 className={'text-center'}>Enter your details and we will create your signature</h4>
+</Col>
+</Row>
+);
+
+export default Header;

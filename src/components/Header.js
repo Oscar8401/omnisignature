3c4ed5bc1d281e085d1e7ec0bb32b981324495b7
Travis LaDuke <travisladuke@gmail.com>
2016-12-13 10:51:33 -0800

    Create basic skeleton of Signature form and preview.
    Preview is in an iframe because iframe have reset css.
    Form data is stored redux via redux-form.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,23 @@
-import React, {
-  Component
-} from 'react';
+import React from 'react';
 import {
   Row,
   Col
 } from 'react-bootstrap';
-import classNames from 'classnames';
 
-export const Header = (state) => (
+
+const Header = (state) => (
   <Row>
-<Col>
-<h1 className={'text-center'}>DotLink</h1>
-</Col>
-<Col>
-<h2 className={'text-center'}>Email Signature Creator</h2>
-</Col>
-<Col>
-<h4 className={'text-center'}>Enter your details and we will create your signature</h4>
-</Col>
-</Row>
+    <Col>
+    <h1 className={ 'text-center' }>DotLink</h1>
+    </Col>
+    <Col>
+    <h2 className={ 'text-center' }>Email Signature Creator</h2>
+    </Col>
+    <Col>
+    <h4 className={ 'text-center' }>Enter your details and we will create your signature</h4>
+    </Col>
+  </Row>
 );
+
 
 export default Header;
---
+++ src/components/SignatureForm.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import {
+  FormGroup,
+  FormControl,
+  ControlLabel
+} from 'react-bootstrap';
+import { connect } from 'react-redux';
+import { Field, reduxForm, getFormValues } from 'redux-form';
+
+
+const Option = (props) => {
+  const {placeholder, type, input, name, label, componentClass, locations} = props;
+  const {value, onChange} = input;
+
+
+  const options = Object.keys(locations).map((k) => {
+    return <option key={ k } value={ locations[k] }> { k } </option>;
+  });
+
+  return (
+    <FormGroup controlId={ name }>
+      <ControlLabel>
+        { label }
+      </ControlLabel>
+      <FormControl placeholder={ placeholder }
+        type={ type }
+        value={ value }
+        componentClass={ componentClass }
+        onChange={ onChange }>
+        { options }
+      </FormControl>
+    </FormGroup>
+    );
+};
+
+const FieldGroup = (props) => {
+  const {placeholder, type, input, name, label, componentClass} = props;
+  const {value, onChange} = input;
+
+  return (
+    <FormGroup controlId={ name }>
+      <ControlLabel>
+        { label }
+      </ControlLabel>
+      <FormControl placeholder={ placeholder }
+        type={ type }
+        value={ value }
+        componentClass={ componentClass }
+        onChange={ onChange }>
+      </FormControl>
+    </FormGroup>
+    );
+};
+
+const SignatureForm = (state) => {
+
+  return (
+    <form>
+      <Field name="fullName"
+        type="text"
+        placeholder='Duncan Crocker'
+        label="Your Name"
+        component={ FieldGroup } />
+      <Field name="email"
+        type="text"
+        placeholder="dcrocker@dotlink.com"
+        label="Email"
+        component={ FieldGroup } />
+      <Field name="location"
+        type="select"
+        label="Location"
+        componentClass="select"
+        locations={ state.formValues ? state.formValues.locations : state.initialValues.locations }
+        component={ Option } />
+      <Field name="title"
+        type="text"
+        placeholder="President"
+        label="Title"
+        component={ FieldGroup } />
+      <Field name="officePhone"
+        type="text"
+        placeholder="123 456 7890"
+        label="Office Phone"
+        component={ FieldGroup } />
+      <Field name="mobilePhone"
+        type="text"
+        placeholder="123 456 7890"
+        label="Mobile Phone"
+        component={ FieldGroup } />
+    </form>
+    );
+};
+
+const initialValues = {
+  fullName: 'Duncan Crocker',
+  title: 'Sales Engineer',
+  email: 'dcrocker@dotlink.com',
+  officePhone: '123 456 7890',
+  mobilePhone: '123 555 7890',
+  locations: {
+      'California': '2161 San Joaquin Hills Road | Newport Beach, CA 92660 ',
+      'Grand Cayman': '3-110 Governors Square|Seven Mile Beach|Grand Cayman, Cayman Islands|1361GT KY1-1108'
+  },
+  location: '2161 San Joaquin Hills Road | Newport Beach, CA 92660 '
+};
+
+
+const mapStateToProps = (state) => {
+  return {
+    formValues: getFormValues('userForm')(state)
+  };
+};
+
+const SignatureForm_ = reduxForm({
+  form: 'userForm',
+  initialValues: initialValues,
+  enableReinitialize: true
+})(SignatureForm);
+
+const ConnectedSignatureForm = connect(mapStateToProps)(SignatureForm_);
+
+export default ConnectedSignatureForm;
+
+
+
